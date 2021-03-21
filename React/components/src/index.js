722487ead81586893d2ad29050ad5bcb3cfb9885
Sachin Kamath <kamathsachin@hotmail.com>
2021-03-21 00:32:30 -0500

    final changes for approval card

--- React/components/src/index.js
+++ React/components/src/index.js
@@ -7,25 +7,30 @@ import faker from "faker";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 6:00 PM"
-        comment="Nice Blog Post"
-        imageSrc={faker.image.animals()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 4:43 PM"
-        comment="Super Blog Post"
-        imageSrc={faker.image.animals()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today at 5:15 PM"
-        comment="Awesome Blog Post"
-        imageSrc={faker.image.animals()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 6:00 PM"
+          comment="Nice Blog Post"
+          imageSrc={faker.image.animals()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 4:43 PM"
+          comment="Super Blog Post"
+          imageSrc={faker.image.animals()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 5:15 PM"
+          comment="Awesome Blog Post"
+          imageSrc={faker.image.animals()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
