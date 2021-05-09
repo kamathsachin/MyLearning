d9cf7e7a6eb5391758720e681f2bbb843d7393fc
Sachin Kamath <kamathsachin@hotmail.com>
2021-05-09 17:02:42 -0500

    Blocking & Non-Blocking calls

--- Node/my-first-node-server/app.js
+++ Node/my-first-node-server/app.js
@@ -16,10 +16,23 @@ const server = http.createServer((req, res) => {
   }
 
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'Dummy Text');
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
+    const body = [];
+
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      // fs.writeFileSync('message.txt', message);
+      fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
   }
 
   res.setHeader('Content-Type', 'text/html');
