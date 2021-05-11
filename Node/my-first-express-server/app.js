374e4e10c4a8630f784d07ef13d5d86f3d38afb4
Sachin Kamath <kamathsachin@hotmail.com>
2021-05-11 00:57:25 -0500

    Added a new middleware with route to products

--- Node/my-first-express-server/app.js
+++ Node/my-first-express-server/app.js
@@ -3,8 +3,12 @@ const express = require('express');
 const app = express();
 app.use('/add-product', (req, res, next) => {
     console.log('In the Add Product Middleware!!');
-    res.send('<h1>Welcome from Add Product!!</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="product"><button>Add Product</button></form>');
 });
+
+app.use('/product', (req, res, next) => {
+    console.log('Entered Add Products');
+})
 
 app.use('/', (req, res, next) => {
     console.log('In the Root Middleware!!');
