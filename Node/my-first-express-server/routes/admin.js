b90648c110915525f214fb169bf5dac3a8ef015b
Sachin Kamath <kamathsachin@hotmail.com>
2021-05-12 01:11:19 -0500

    added html files

--- Node/my-first-express-server/routes/admin.js
+++ Node/my-first-express-server/routes/admin.js
@@ -1,18 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send(
-        '<form action="/admin/add-product" method="POST"><input type="text" name="product"><button>Add Product</button></form>'
-    );
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // app.get will filter only GET request
 // app.post will filter only POST request
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+  console.log(req.body);
+  res.redirect('/');
 });
 
 module.exports = router;
