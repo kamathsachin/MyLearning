c518e37c0c66523190128162b3f6a295f47b255b
Sachin Kamath <kamathsachin@hotmail.com>
2021-05-12 00:34:33 -0500

    implemented path filtering

--- Node/my-first-express-server/routes/admin.js
+++ Node/my-first-express-server/routes/admin.js
@@ -3,12 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="product"><button>Add Product</button></form>');
+    res.send(
+        '<form action="/admin/add-product" method="POST"><input type="text" name="product"><button>Add Product</button></form>'
+    );
 });
 
 // app.get will filter only GET request
 // app.post will filter only POST request
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
