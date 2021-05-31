a3770ea971390d769ff85c416914ae1d4485206b
Sachin Kamath <kamathsachin@hotmail.com>
2021-05-31 04:41:26 -0500

    Added append, prepend and printlist methods

--- DS + Algo/LinkedList/LinkedList.js
+++ DS + Algo/LinkedList/LinkedList.js
@@ -6,8 +6,48 @@ class LinkedList {
     };
 
     this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+
+    return array;
   }
 }
 
 const linkedList = new LinkedList(10);
-console.log(linkedList);
+linkedList.append(10);
+linkedList.append(20);
+linkedList.append(30);
+linkedList.prepend(1);
+console.log(linkedList.printList());
