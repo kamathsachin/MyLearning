ab96738a0c7f4aff1ae45af04301704ee922920c
Sachin Kamath <kamathsachin@hotmail.com>
2021-05-31 14:37:47 -0500

    Implemented Doubly Linked List

---
+++ DS + Algo/LinkedList/DoublyLinkedList.js
@@ -0,0 +1,102 @@
+class DoublyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+      prev: null,
+    };
+
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+      prev: null,
+    };
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null,
+      prev: null,
+    };
+
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = {
+      value: value,
+      next: null,
+      prev: null,
+    };
+
+    const leader = this.traverseToIndex(index - 1);
+    const follower = leader.next;
+    leader.next = newNode;
+    newNode.prev = leader;
+    newNode.next = follower;
+    follower.prev = newNode;
+    this.length++;
+    this.printList();
+  }
+
+  remove(index) {
+    const leader = this.traverseToIndex(index - 1);
+    const unWantedNode = leader.next;
+    const follower = unWantedNode.next;
+    leader.next = follower;
+    follower.prev = leader;
+    this.length--;
+    return console.log(this.printList());
+  }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+
+    return currentNode;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+
+    return array;
+  }
+}
+
+const linkedList = new DoublyLinkedList(5);
+linkedList.append(10);
+linkedList.append(20);
+linkedList.append(30);
+linkedList.prepend(1);
+linkedList.insert(3, 15);
+linkedList.remove(3);
+console.log(linkedList.printList());
