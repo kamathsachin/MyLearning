1b6ea05abb095039ba25b3e7fe9be8e2102fbfb4
Sachin Kamath <kamathsachin@hotmail.com>
2021-07-10 09:14:05 -0500

    Implemented Counters

--- React/shopping-cart-simulator/src/components/counter.jsx
+++ React/shopping-cart-simulator/src/components/counter.jsx
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = { count: 0 };
-
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
         </button>
       </div>
     );
@@ -23,12 +23,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = 'badge m-2 badge-';
-    return (classes += this.state.count === 0 ? 'warning' : 'primary');
+    return (classes += this.props.counter.value === 0 ? 'warning' : 'primary');
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
+    const { value } = this.props.counter;
+    return value === 0 ? 'Zero' : value;
   }
 }
 
