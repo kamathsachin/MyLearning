cbab12982a14fc0e0c60f33682bbb507add78be9
Sachin Kamath <kamathsachin@hotmail.com>
2021-07-10 10:20:42 -0500

    Converted counters component to a controlled component

---
+++ React/shopping-cart-simulator/src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Navbar
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
